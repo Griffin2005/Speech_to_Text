b2e87b54a008ca5a6ce35902e52f185174dcae48
Harsh Maurya <136671411+Griffin2005@users.noreply.github.com>
2024-12-28 11:30:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+openaikey = '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'
+my_secret = os.environ['openaikey']
